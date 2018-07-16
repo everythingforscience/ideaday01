60541f8442c4ab66a41144e375623fea8f33c7e6
everythingforscience <yanghuidong1994@outlook.com>
2018-07-16 15:26:16 +0800

    idea上传githubTest 修改后Test2

--- src/test/java/flow/FlowBeanTest.java
+++ src/test/java/flow/FlowBeanTest.java
@@ -5,5 +5,6 @@ import static org.testng.Assert.*;
 public class FlowBeanTest {
     public static void main(String [] args){
         System.out.print("science");
+        System.out.print("science");
     }
 }
